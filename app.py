891633167c1d65a6844faab0ab288c085c896b64
Manuel-BMSI <manuel.strychalski@bmsiml.org>
2024-05-24 19:46:17 +0200

    choix du theme en volet gauche

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ st.write("""
 Spaced Repetition System SQL practice
 """)
 
+with st.sidebar:
+    option = st.selectbox(
+        "What do like to review ?",
+        ("Joins", "GroupBy", "Windows Functions"),
+        index=None,
+        placeholder="Select a theme ...",
+    )
 
-option = st.selectbox(
-    "What do like to review ?",
-    ("Joins", "GroupBy", "Windows Functions"),
-    index=None,
-    placeholder="Select a theme ...",
-)
-
-st.write('You selected : ', option)
+    st.write('You selected : ', option)
 
 data = {"a" : [1, 2, 3], "b" : [4, 5, 6]}
 df = pd.DataFrame(data)
