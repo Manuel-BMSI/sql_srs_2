48b93099f68baf2977a8bc153ecb03af56c23b8a
Manuel-BMSI <manuel.strychalski@bmsiml.org>
2024-05-25 10:27:48 +0200

    ajout comparaison nb de lignes et colonnes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import duckdb
 import io
 
+#création du df beverages
 csv = """
 beverage,price
 orange juice,2.5
@@ -12,6 +13,7 @@ Tea,3
 beverages = pd.read_csv(io.StringIO(csv))
 #con.execute("CREATE TABLE IF NOT EXISTS beverages AS SELECT * FROM beverages")
 
+#création du df food_items
 csv2 = """
 food_item,food_price
 cookie juice,2.5
@@ -21,6 +23,7 @@ muffin,3
 food_items = pd.read_csv(io.StringIO(csv2))
 #con.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
 
+#correction
 answer = """
 SELECT * FROM beverages
 CROSS JOIN food_items
@@ -40,23 +43,33 @@ with st.sidebar:
         placeholder="Select a theme ...",
     )
 
-    st.write('You selected : ', option)
+    st.write('Votre sélection : ', option)
 
 data = {"a" : [1, 2, 3], "b" : [4, 5, 6]}
 df = pd.DataFrame(data)
 
 # Création d'une relation DuckDB à partir du DataFrame
 relation = duckdb.from_df(df)
-sql_query = st.text_area(label="Entrez votre code ici :")
+sql_query = st.text_area(label="Entrez votre code ici :", key="user_input")
 if sql_query.strip():  # Ensure the query is not empty
     try:
         result = relation.query("data", sql_query).df()
-        st.write(f"Vous avez entré la query suivante : {sql_query}")
+        st.markdown(f"Vous avez entré lme code suivant :\n\n {sql_query}\n\n Voici le résultat de cette requête : \n",
+                    unsafe_allow_html=False)
         st.dataframe(result)
+        if len(result.columns) != len(solution.columns):
+            st.write("Nb de colonnes différent")
+        else: st.write("Nb de colonnes OK")
+
+        n_lines_diff = result.shape[0] - solution.shape[0]
+        if n_lines_diff != 0:
+            st.write(f"Le résultat de votre requête comporte  {n_lines_diff} lignes de différences avec celui de la solution")
+        else: st.write("Nb de lignes OK")
+
     except Exception as e:
         st.write("Error executing query: ", e)
 else:
-    st.write("Please enter a valid SQL query.")
+    st.write("Veuillez entre votre code SQL ci-dessus.")
 
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
